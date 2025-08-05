2d27aacdc33433b4470c915a45206fdaa26b13cb
niin20152-develop <niin20152@gmail.com>
2025-08-05 11:33:34 +0700

    first commit

---
+++ exercise.js
@@ -0,0 +1,15 @@
+//Ex1
+let friend_name="Kimsorng";
+let greeting="hello";
+let message=greeting+","+friend_name;
+console.log(message);
+//Ex2
+const currentYear = 2025;
+const birthYear = 2007;
+let age=currentYear-birthYear;
+console.log(`You are ${age} years old`);
+//Ex3
+let favoriteFoods=["Amok", "Kuy teav", "bai sach sach chrouk"]
+for(let i=0; i<3; i++){
+    console.log(`I love to eat`+ favoriteFoods[i]+`!`);
+}
---
+++ first.js
@@ -0,0 +1,8 @@
+// for (let i=0; i<1000; i++){
+//     if(i%2==0){
+//         console.log("hello");
+//     }else{
+//         console.log("hi");
+//     }
+// }
+for(let i=0; i<5; i++){console.log(i)}
